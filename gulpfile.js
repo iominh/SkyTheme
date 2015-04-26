8bd00eaeff4ad2606d193595d4856ab1fc7e2de2
minh nguyen <minh.9.nguyen@gmail.com>
2015-04-26 17:20:47 -0400

    add footer, pagination, article previews

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,62 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync');
-var sass        = require('gulp-sass');
-var prefix      = require('gulp-autoprefixer');
-var cp          = require('child_process');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var cp = require('child_process');
 var replace = require('gulp-regex-replace');
 var rename = require("gulp-rename");
 
 var messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+  jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build',
+  jekyllClean: '<span style="color: grey">Running:</span> $ jekyll clean'
 };
 
 /**
  * Build the Jekyll Site
  */
 gulp.task('jekyll-build', function (done) {
-    browserSync.notify(messages.jekyllBuild);
-    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
-        .on('close', done);
+  browserSync.notify(messages.jekyllBuild);
+  return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+    .on('close', done);
+});
+
+/**
+ * Clean the Jekyll Site
+ */
+gulp.task('jekyll-clean', function (done) {
+  browserSync.notify(messages.jekyllClean);
+  return cp.spawn('jekyll', ['clean'], {stdio: 'inherit'})
+    .on('close', done);
 });
 
 /**
  * Rebuild Jekyll & do page reload
  */
 gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-    browserSync.reload();
+  browserSync.reload();
 });
 
 /**
  * Wait for jekyll-build, then launch the Server
  */
-gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
-    browserSync({
-        server: {
-            baseDir: '_site'
-        }
-    });
+gulp.task('browser-sync', ['sass', 'jekyll-build'], function () {
+  browserSync({
+    server: {
+      baseDir: '_site'
+    }
+  });
 });
 
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
 gulp.task('sass', function () {
-    return gulp.src('./_scss/**/*.scss')
-        .pipe(sass())
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/css'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('css'));
+  return gulp.src('./_scss/**/*.scss')
+    .pipe(sass())
+    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(gulp.dest('_site/css'))
+    .pipe(browserSync.reload({stream: true}))
+    .pipe(gulp.dest('css'));
 });
 
 /**
@@ -54,8 +64,8 @@ gulp.task('sass', function () {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('_scss/**/*.scss', ['sass']);
-    gulp.watch(['index.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+  gulp.watch('_scss/**/*.scss', ['sass']);
+  gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
 
@@ -73,3 +83,4 @@ gulp.task('replace', function () {
  * compile the jekyll site, launch BrowserSync & watch files.
  */
 gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('clean', ['jekyll-clean']);
